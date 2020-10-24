b52a76277d70fd9fc695c4dce115cf1950442ad2
Oleksandr <fight3d@gmail.com>
2020-10-24 13:47:43 +0300

    assignment3-task1

--- HomeWork04/ViewController.swift
+++ HomeWork04/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        task1(name: "Aleksandr")
     }
 
+    func task1(name: String){
+        let amount = name.count
+        print ("Name \(name) contains \(amount) characters")
+    }
 
 }
 
