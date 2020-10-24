ff5534385f0e665892700664d3e7a2999670e659
Oleksandr <fight3d@gmail.com>
2020-10-24 16:40:15 +0300

    assignment3-task4

--- HomeWork04/ViewController.swift
+++ HomeWork04/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
         
  //       task1(name: "Aleksandr")
 //        task2(patronymic: "Геннадьевич")
-        task3(name: "AlexYatsenko")
+//        task3(name: "AlexYatsenko")
+        task4(word: "Ось")
     }
     
 //Задача 1. Создать строку с своим именем, вывести количество символов содержащихся в ней.
@@ -36,18 +37,27 @@ class ViewController: UIViewController {
 //    Задача 3. Cоздать строку, где слитно написано Ваши ИмяФамилия “IvanVasilevich»
 //    Вам нужно разбить на две отдельных строки из предыдущей создать третью, где они обе будут разделены пробелом
     
-    func task3(name: String){
-        var firstName = ""
-        var secondName = ""
-        name.enumerated().forEach {index, char in
-            if char.isUppercase, index != 0 {
-                firstName = String(name.prefix(index))
-                secondName = String(name.dropFirst(index))
-                return
-            }
-            
+//    func task3(name: String){
+//        var firstName = ""
+//        var secondName = ""
+//        name.enumerated().forEach {index, char in
+//            if char.isUppercase, index != 0 {
+//                firstName = String(name.prefix(index))
+//                secondName = String(name.dropFirst(index))
+//                return
+//            }
+//
+//        }
+//        print (firstName, secondName)
+//    }
+    
+    //Задача 4. Вывести строку зеркально Ось → ьсО не используя reverse (посимвольно)
+    func task4 (word: String){
+        var reversed = ""
+        for index in word.indices {
+            reversed.insert(word[index], at: word.startIndex)
         }
-        print (firstName, secondName)
+        print("\(word) -> \(reversed)")
     }
 }
 
