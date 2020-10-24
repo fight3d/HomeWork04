35f97f26dd397fc76073d74069bffdf6de7ff775
Oleksandr <fight3d@gmail.com>
2020-10-24 16:26:17 +0300

    assignment3-task3

--- HomeWork04/ViewController.swift
+++ HomeWork04/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
  //       task1(name: "Aleksandr")
-        task2(patronymic: "Геннадьевич")
+//        task2(patronymic: "Геннадьевич")
+        task3(name: "AlexYatsenko")
     }
     
 //Задача 1. Создать строку с своим именем, вывести количество символов содержащихся в ней.
@@ -24,13 +25,30 @@ class ViewController: UIViewController {
 //    }
 
     //Задача 2. Создать строку с своим отчеством проверить его на окончание “ич/на”
-    func task2(patronymic: String){
-        if patronymic.hasSuffix("ич") || patronymic.hasSuffix("на") {
-            print("Patronymic \(patronymic) contains ич/на")
-        } else {
-            print ("Patromymic \(patronymic) doesn't contain ич/на")
-        }
-    }
+//    func task2(patronymic: String){
+//        if patronymic.hasSuffix("ич") || patronymic.hasSuffix("на") {
+//            print("Patronymic \(patronymic) contains ич/на")
+//        } else {
+//            print ("Patromymic \(patronymic) doesn't contain ич/на")
+//        }
+//    }
     
+//    Задача 3. Cоздать строку, где слитно написано Ваши ИмяФамилия “IvanVasilevich»
+//    Вам нужно разбить на две отдельных строки из предыдущей создать третью, где они обе будут разделены пробелом
+    
+    func task3(name: String){
+        var firstName = ""
+        var secondName = ""
+        name.enumerated().forEach {index, char in
+            if char.isUppercase, index != 0 {
+                firstName = String(name.prefix(index))
+                secondName = String(name.dropFirst(index))
+                return
+            }
+            
+        }
+        print (firstName, secondName)
+    }
 }
+
 
